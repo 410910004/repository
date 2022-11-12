7e250d8f4b1f2803f3a3d583c09a40c0e6251f2c
410910004 <114295411+410910004@users.noreply.github.com>
2022-11-12 15:35:12 +0800

    Delete Vue尚硅谷前台电商项目 directory

--- Vue尚硅谷前台电商项目/src/Mock/mockServer.js
+++
@@ -1,7 +0,0 @@
-//引入mock
-import Mock from 'mockjs';
-import banner from './banner';
-import floor from './floor';
-
-Mock.mock('/Mock/banner',{code:200,data:banner});
-Mock.mock('/Mock/floor',{code:200,data:floor})
